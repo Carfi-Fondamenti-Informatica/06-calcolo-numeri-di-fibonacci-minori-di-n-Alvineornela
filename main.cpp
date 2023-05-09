c104b4a78e6912372114b82c930be857048a6799
Alvineornela <127629007+Alvineornela@users.noreply.github.com>
2023-05-09 15:58:40 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,16 @@
 using namespace std;
 
 int main() {
+    int n_sf=1;
+    int add=0;
+    int n_psf;
+    int n;
+    cin>>n;
+    do{
+        cout<<n_sf<<endl;
+        n_psf=n_sf;
+        n_sf=n_sf+add;
+        add=n_psf;
+    }while(n_sf<=n);
    return 0;
 }
